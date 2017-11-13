6d0606a3a06af6063f3a13536f67cecbbeb5d774
maryli891 <marytta095@gmail.com>
2017-11-13 19:22:25 -0300

    Se crea la nueva clase bucles1

---
+++ src/bucles1/Bucles1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bucles1;
+
+/**
+ *
+ * @author LC22
+ */
+public class Bucles1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
