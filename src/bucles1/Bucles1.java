ce5030e267423eb795dda64b170e6ecad4800245
maryli891 <marytta095@gmail.com>
2017-11-13 19:28:52 -0300

    Se realizo ejercicio bucles1

--- src/bucles1/Bucles1.java
+++ src/bucles1/Bucles1.java
@@ -14,8 +14,17 @@ public class Bucles1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        int stop = -1;
+        int index =0;
+        
+        while(index<=100) //Mientras se cumpla esta condicion
+        {
+            //stop = Integer.parseInt(JOptionPane.showInputDialog
+            System.out.println(index);
+        }
+        //TODO code application logic here
     }
     
 }
